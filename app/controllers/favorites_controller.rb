adf9055f75bab7f9d4fdb388c47330caf78327de
Kierancz <kieran.czerwinski@colorado.edu>
2014-11-09 18:05:26 -0700

    small fix

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,38 +1,20 @@
 class FavoritesController < ApplicationController
-	#before_action :set_course
 	before_action :set_model
-
-	#def create
-	#	if Favorite.create(favoritable: @course, user: current_user)
-	#		redirect_to @course, notice: 'Course has been favorited'
-	#	else
-	#		redirect_to @course, alert: 'Something went wrong... check back later'
-	#	end
-	#end
 
 	def create
 		if Favorite.create(favoritable: @model, user: current_user)
-			redirect_to :back, notice: " #{@model.title} has been favorited!"
+			redirect_to	request.referer + '#posts', notice: " #{@model.title} has been favorited!"
 		else
 			redirect_to :back, alert: 'Something went wrong... check back later'
 		end
 	end
 
-	#def destroy
-	#	Favorite.where(favoritable_id: @course.id, user_id: current_user.id).first.destroy
-	#	redirect_to @course, notice: 'Course is no longer in favorites'
-	#end
-
 	def destroy
 		Favorite.where(favoritable_id: @model.id, user_id: current_user.id).first.destroy
-		redirect_to :back, notice: "#{@model.title} is no longer in favorites."
+		redirect_to request.referer + '#posts', notice: "#{@model.title} is no longer in favorites."
 	end
 
 	private
-
-	#def set_course
-	#	@course = Course.find(params[:course_id] || params[:id])
-	#nd
 
 	def set_model
 		params.each do |name, value|

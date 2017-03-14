aec529bf0b7c819fe17a53cc0dadecf1cab1a35a
Kierancz <kieran.czerwinski@colorado.edu>
2017-03-13 21:11:01 -0600

    updated gemfile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
-//= require bootstrap-wysihtml5/b3
+//= require bootstrap-wysihtml5
 //= require bootstrap-wysihtml5/locales/es-ES
 //= require_tree .
 //= require jquery.validate
